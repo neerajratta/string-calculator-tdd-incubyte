575c7f6ebc2a9850fe7660a708e3789eb2b4c966
neeraj ratta <neerajratta65@gmail.com>
2025-10-01 23:27:57 +0530

    test: Add failing test for single number

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,7 +1,13 @@
 const { add } = require('../src/stringCalculator');
 
 describe('String Calculator', () => {
-  test('returns 0 for empty string', () => {
-    expect(add('')).toBe(0);
-  });
+    test('returns 0 for empty string', () => {
+        expect(add('')).toBe(0);
+    });
+
+    // New test case for single number
+    test('returns number itself for single number', () => {
+        expect(add('5')).toBe(5);
+    });
+
 });
