85ebc098b438c2893e60e93457555515c5cbbc12
neeraj ratta <neerajratta65@gmail.com>
2025-10-01 23:53:48 +0530

    feat: Implement new line separator support

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,7 @@
 function add(numbers) {
   if (!numbers) return 0;
+    
+    numbers = numbers.replace(/\n/g, ',');
     // single, two or multiple numbers handled in one line
     return numbers.split(',').reduce((sum, n) => sum + Number(n), 0);
 }
