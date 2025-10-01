3562260f94b2536b4b04823353674b65214aac4c
neeraj ratta <neerajratta65@gmail.com>
2025-10-01 23:44:28 +0530

    feat: Refactor add() to handle single and multiple numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,6 @@
 function add(numbers) {
   if (!numbers) return 0;
+    // single, two or multiple numbers handled in one line
     return numbers.split(',').reduce((sum, n) => sum + Number(n), 0);
 }
 module.exports = { add };
